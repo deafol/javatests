2d885f5bb4d72031a13df3700a695590ffc2237e
Milan de Vries <milan.de.vries@ing.nl>
2014-12-16 16:42:12 +0100

    jks tests

--- src/main/java/nl/vinyamar/jks/testJKS.java
+++ src/main/java/nl/vinyamar/jks/testJKS.java
@@ -1,13 +1,19 @@
 package nl.vinyamar.jks;
 
+
+import org.springframework.core.io.ClassPathResource;
+
 import java.io.FileInputStream;
-import java.security.*;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.cert.Certificate;
 
 public class testJKS {
 
     public static void main(String[] args) throws Exception {
-        FileInputStream is = new FileInputStream("/home/vf65fn/Projects/Javatests/resources/store.jks");
+        FileInputStream is = new FileInputStream(new ClassPathResource("store.jks").getFile());
 
         KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
         keystore.load(is, "password".toCharArray());
